ba849cdfa735efcf3147a9352739c0c77e5d97f6
Constantine Kiriaze <ckiriaze@gmail.com>
2014-08-01 16:48:55 -0700

    permissions fixed

--- wp-config.php
+++ wp-config.php
@@ -16,7 +16,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'andydavis_db');
+define('DB_NAME', 'pressplay_db');
 
 /** MySQL database username */
 define('DB_USER', 'root');
